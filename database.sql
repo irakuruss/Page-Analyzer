4bf678e60841dac5e7692d5854977d11e528a5ac
Irina Kurus <132392842+irakuruss@users.noreply.github.com>
2023-11-15 22:57:27 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS url_checks;
 CREATE TABLE urls (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name varchar(255) UNIQUE,
-  created_at date
+  created_at date NOT NULL
 );
 
 CREATE TABLE url_checks (
@@ -15,5 +15,5 @@ CREATE TABLE url_checks (
   h1 varchar(255),
   title varchar(255),
   description varchar(255),
-  created_at date
+  created_at date NOT NULL
 );
